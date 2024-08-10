f58f638e6ea4c6e284094c7beb16772458d3fc07
Ice-CubeXQ <dealplug72@gmail.com>
2024-08-10 15:01:19 +0300

    Added pagination functionality

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -10,19 +10,41 @@ class CharList extends Component {
     charList: [],
     loading: true,
     error: false,
+    newItemLoading: false,
+    offset: 210,
+    charEnded: false,
   };
 
   marvelService = new MarvelService();
 
   componentDidMount() {
-    this.marvelService.getAllCharacters().then(this.onCharactersLoad).catch(this.onError);
+    this.onRequest();
   }
 
-  onCharactersLoad = (charList) => {
+  onRequest = (offset) => {
+    this.onCharListLoading();
+    this.marvelService.getAllCharacters(offset).then(this.onCharactersLoad).catch(this.onError);
+  };
+
+  onCharListLoading = () => {
     this.setState({
-      charList,
-      loading: false,
+      newItemLoading: true,
     });
+  };
+
+  onCharactersLoad = (newCharList) => {
+    let ended = false;
+    if (newCharList.length < 9) {
+      ended = true;
+    }
+
+    this.setState(({ offset, charList }) => ({
+      charList: [...charList, ...newCharList],
+      loading: false,
+      newItemLoading: false,
+      offset: offset + 9,
+      charEnded: ended,
+    }));
   };
 
   onError = () => {
@@ -52,7 +74,7 @@ class CharList extends Component {
   }
 
   render() {
-    const { charList, loading, error } = this.state;
+    const { charList, loading, error, newItemLoading, offset, charEnded } = this.state;
 
     const items = this.renderItems(charList);
 
@@ -65,7 +87,7 @@ class CharList extends Component {
         {errorMessage}
         {spiner}
         {content}
-        <button className="button button__main button__long">
+        <button disabled={newItemLoading} style={{ display: charEnded ? "none" : "block" }} onClick={() => this.onRequest(offset)} className="button button__main button__long">
           <div className="inner">load more</div>
         </button>
       </div>
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,6 +1,7 @@
 class MarvelService {
   _apiBase = "https://gateway.marvel.com:443/v1/public/";
   _apiKey = "apikey=23306ca8a4adb99aba65d096c093d5a8";
+  _baseOffset = 210;
 
   getResourse = async (url) => {
     let res = await fetch(url);
@@ -12,8 +13,8 @@ class MarvelService {
     return await res.json();
   };
 
-  getAllCharacters = async () => {
-    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  getAllCharacters = async (offset = this._baseOffset) => {
+    const res = await this.getResourse(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
     return res.data.results.map(this._transformCharacter);
   };
   getCharacter = async (id) => {
