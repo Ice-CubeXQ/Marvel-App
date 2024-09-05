35ebc51a8cb4f531fd18356a38f318e3f270cdcd
Ice-CubeXQ <dealplug72@gmail.com>
2024-09-05 13:03:22 +0300

    Refactor: Consolidate repeated logic using custom hooks

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -3,49 +3,38 @@ import PropTypes from "prop-types";
 
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
-import MarvelService from "../../services/MarvelService";
+import useMarvelService from "../../services/MarvelService";
 
 import "./charList.scss";
 
 const CharList = (props) => {
   const [charList, setCharList] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
   const [newItemLoading, setNewItemLoading] = useState(false);
   const [offset, setOffset] = useState(210);
   const [charEnded, setCharEnded] = useState(false);
 
-  const marvelService = new MarvelService();
+  const { loading, error, getAllCharacters } = useMarvelService();
 
   useEffect(() => {
-    onRequest();
+    onRequest(offset, true);
   }, []);
 
-  const onRequest = (offset) => {
-    onCharListLoading();
-    marvelService.getAllCharacters(offset).then(onCharactersLoad).catch(onError);
+  const onRequest = (offset, initial) => {
+    initial ? setNewItemLoading(false) : setNewItemLoading(true);
+    getAllCharacters(offset).then(onCharListLoaded);
   };
 
-  const onCharListLoading = () => {
-    setNewItemLoading(true);
-  };
-
-  const onCharactersLoad = (newCharList) => {
+  const onCharListLoaded = (newCharList) => {
     let ended = false;
     if (newCharList.length < 9) {
       ended = true;
     }
 
     setCharList((charList) => [...charList, ...newCharList]);
-    setLoading(false);
+
     setNewItemLoading((newItemLoading) => false);
     setOffset((offset) => offset + 9);
     setCharEnded((charEnded) => ended);
-  };
-
-  const onError = () => {
-    setError(true);
-    setLoading(false);
   };
 
   const itemRefs = useRef([]);
@@ -97,14 +86,13 @@ const CharList = (props) => {
   const items = renderItems(charList);
 
   const errorMessage = error ? <ErrorMessage /> : null;
-  const spiner = loading ? <Spinner /> : null;
-  const content = !(loading || error) ? items : null;
+  const spiner = loading && !newItemLoading ? <Spinner /> : null;
 
   return (
     <div className="char__list">
       {errorMessage}
       {spiner}
-      {content}
+      {items}
       <button disabled={newItemLoading} style={{ display: charEnded ? "none" : "block" }} onClick={() => onRequest(offset)} className="button button__main button__long">
         <div className="inner">load more</div>
       </button>
