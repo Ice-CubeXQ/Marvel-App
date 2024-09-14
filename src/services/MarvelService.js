3988405462233bf949204cf8a5d75e1accc26672
Ice-CubeXQ <dealplug72@gmail.com>
2024-09-14 19:23:08 +0300

    Added a form to search for a character by name

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -7,6 +7,11 @@ const useMarvelService = () => {
   const _apiKey = "apikey=23306ca8a4adb99aba65d096c093d5a8";
   const _baseOffset = 210;
 
+  const getCharacterByName = async (name) => {
+    const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+    return res.data.results.map(_transformCharacter);
+  };
+
   const getAllCharacters = async (offset = _baseOffset) => {
     const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
     return res.data.results.map(_transformCharacter);
@@ -50,7 +55,7 @@ const useMarvelService = () => {
     };
   };
 
-  return { loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic };
+  return { loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic, getCharacterByName };
 };
 
 export default useMarvelService;
